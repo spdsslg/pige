bcfb57d7f267e8e7d1e97aaa525d259c48555786
michael <leqj5656@gmail.com>
2026-04-26 00:42:00 +0300

    Almost finished tutorial

---
+++ WpfTutorial/WpfTutorial/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTutorial;
+
+public enum Gender
+{
+    Male,
+    Female
+}
+
+public class Contact
+{
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public Gender Gender { get; set; }
+    public Contact() { }
+}
---
+++ WpfTutorial/WpfTutorial/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Contact> contacts { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            contacts = new ObservableCollection<Contact>();
+            DataContext = contacts;
+        }
+
+        private void MenuItem_Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_AddContact(object sender, RoutedEventArgs e)
+        {
+            Opacity = 0.5;
+
+            AddContactWindow contactWindow = new AddContactWindow();
+            if (contactWindow.ShowDialog().Value == true)
+            {
+                contacts.Add(contactWindow.NewContact);
+            }
+
+            Opacity = 1.0;
+        }
+
+        private void MenuItem_ClearContacts(object sender, RoutedEventArgs e)
+        {
+            contacts.Clear();
+        }
+
+        private void MenuItem_About(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is simple contacts manager.", "Contact Manager",MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
